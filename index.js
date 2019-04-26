f5608674039c490cde83bfbcab134426f5e9ccaf
Lei Wang <xwang@sandiego.edu>
2019-04-25 20:11:53 -0700

    added basic skeleton framework for search function of app

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+'use strict';
 
+
+
+
+function watchForm() {
+    console.log('watchForm ran');
+    $('form').submit(event =>  {
+        event.preventDefault();
+        const codeSearchTerm = $('#js-code-search').val();
+    });
+}
+
+$(watchForm);
