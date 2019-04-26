bb2064364e8b333b75e0650dc4e8c28b9bb0e0af
Lei Wang <xwang@sandiego.edu>
2019-04-25 21:43:50 -0700

    implemented YouTube search results for educational category

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
 'use strict';
 
+const youTubeAPIKey = 'AIzaSyDxEA4w7rd0YACNoOzUeSK3YaI_UON9zjw';
+const youTubeSearchURL = 'https://www.googleapis.com/youtube/v3/search';
 
+
+function formatYouTubeQuery(parameters) {
+    const queryItems = Object.keys(parameters)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(parameters[key])}`)
+    return queryItems.join('&');
+  }
+
+function displayYouTubeResults(responseJson) {
+  // if there are previous results, remove them
+  console.log(responseJson);
+  const youTubeResults = responseJson.items;
+//  $('#results-list').empty();
+  // iterate through the items array
+  const appendYouTubeResults = youTubeResults.map(video => {
+      return (
+        `<li><h3>${video.snippet.title}</h3>
+        <p>${video.snippet.description}</p>
+        <img src='${video.snippet.thumbnails.default.url}'>
+        </li>`
+      );
+  });
+  $('#results-list').html(appendYouTubeResults);
+}
+
+/*  // embed YouTube video (replace videoID)
+<iframe width="560" height="315" src="https://www.youtube.com/embed/N4mEzFDjqtA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+*/
+
+function getYouTubeVideos(query) {
+  const parameters = {
+    key: youTubeAPIKey,
+    q: query,
+    part: 'snippet',
+    maxResults: 20,
+    type: 'video',
+    videoCategoryId: '27'
+  };
+  const youTubeQueryString = formatYouTubeQuery(parameters)
+  const youTubeURL = youTubeSearchURL + '?' + youTubeQueryString;
+
+  console.log(youTubeURL);
+
+  fetch(youTubeURL)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayYouTubeResults(responseJson))
+    .catch(error => {
+      $('#js-error-message').text(`Something went wrong: ${error.message}`);
+    });
+}
 
 
 function watchForm() {
@@ -8,6 +64,7 @@ function watchForm() {
     $('form').submit(event =>  {
         event.preventDefault();
         const codeSearchTerm = $('#js-code-search').val();
+        getYouTubeVideos(codeSearchTerm);
     });
 }
 
